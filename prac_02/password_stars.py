072699ede55298142f98286cf5fc619fddb94865
Edmond <edmond.xie@my.jcu.edu.au>
2025-09-29 01:50:42 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,5 +1,12 @@
-password = input("Password: ")
-while len(password) != 10:
-    print("Password not work")
+def main()
+    password = get_password()
+
+def get_password():
     password = input("Password: ")
-print('*' * len(password))
+    while len(password) != 10:
+        print("Password not work")
+        password = input("Password: ")
+    print('*' * len(password))
+
+
+main()
