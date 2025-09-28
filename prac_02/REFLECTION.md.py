4505dfcfe79c20fbea8894e736244bd30b53782c
Edmond <edmond.xie@my.jcu.edu.au>
2025-09-29 01:45:58 +1000

    Reflection

---
+++ prac_02/REFLECTION.md.py
@@ -0,0 +1,19 @@
+# CP1404 Practical Reflection
+
+## What percentage of the lecture videos have you watched so far?
+#0%. I have not been very consistent
+
+## What time, location and duration have you scheduled to watch lecture videos?
+#So far I have not scheduled time.
+
+## What are you doing well in the subject so far that you will keep doing?
+#There is not much to consider on what to keep doing.
+
+## What do you need to stop or change in your work for this subject overall so far?
+#I should change the way I work better in multiple ways like start to watch the videos.
+
+## What are you currently doing poorly for practicals in this subject that you will change?
+#The biggest thing I need to consider is reading the question more thoroughly
+
+## What are you doing really well for practicals in this subject?
+#I think I can do the coding well but I need to get used to the conventions more.
