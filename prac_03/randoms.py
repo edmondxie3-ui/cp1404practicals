8c825cbc226dee6f38cc26e84e43dd2c8e4c47ff
Edmond <edmond.xie@my.jcu.edu.au>
2025-10-05 04:53:30 +1000

    Display different 'random' commands.

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -1,15 +1,16 @@
-#print(random.randint(5, 20))
+# a. print(random.randint(5, 20))
 #Line 1 showed a '6'
 #Smallest number that could display was 5 and 20 being the largest.
 
-#print(random.randrange(3, 10, 2))
+# b. print(random.randrange(3, 10, 2))
 #Line 2 showed a '3'
 #Smallest number that could display was 3 and 10 being the largest.
 #No, range has an increment of 2. Numbers that could be picked are 3, 5, 7, 9. 4 gets skipped from the increment.
 
-#print(random.uniform(2.5, 5.5))
+# c. print(random.uniform(2.5, 5.5))
 #Line 3 shows a '3.1920043980383457'
 #Smalled number that could display was a 2.5 and 5.5 being the largest.
 
+# d.
 import random
 print(random.randint(1,100))
