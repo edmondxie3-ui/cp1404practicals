5461c3c323b23b84728d829849fd18d6404fa3bc
Edmond <edmond.xie@my.jcu.edu.au>
2025-10-12 00:07:43 +1000

    Display different 'random' commands.

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,16 @@
+# numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# a. 3 Correct
+# b. 2 Correct
+# c. 1 Correct
+# d. 9, 5, 1, 4, 1, 3 Incorrect (Other way around)
+# e. 1, 5 Incorrect
+# f. True Correct
+# g. False Correct
+# h. True Correct
+# i. 3, 1, 4, 1, 5, 9, 2, 6, 5, 3 Correct
+
+numbers = ["ten", 1, 4, 1, 5, 9, 1]
+
+print(numbers[2:])
+print(9 in numbers)
