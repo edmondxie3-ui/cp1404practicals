cd1035b550bbcda94892105331ec7af39f721d61
Edmond <edmond.xie@my.jcu.edu.au>
2025-10-12 22:36:02 +1000

    Displays items from a list

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,20 @@
+# numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# a. 3 Correct
+# b. 2 Correct
+# c. 1 Correct
+# d. 9, 5, 1, 4, 1, 3 Incorrect (Other way around)
+# e. 1, 5 Incorrect
+# f. True Correct
+# g. False Correct
+# h. True Correct
+# i. 3, 1, 4, 1, 5, 9, 2, 6, 5, 3 Correct
+
+# 1. and 2.
+numbers = ["ten", 1, 4, 1, 5, 9, 1]
+
+# 3.
+print(numbers[2:])
+
+# 4.
+print(9 in numbers)
