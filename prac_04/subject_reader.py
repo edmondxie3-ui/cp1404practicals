602e2ceb102326fdf69cc680542aeee508193ec5
Edmond <edmond.xie@my.jcu.edu.au>
2025-10-12 23:12:05 +1000

    Display details from a text file.

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,21 +9,24 @@ FILENAME = "subject_data.txt"
 def main():
     data = load_data(FILENAME)
     print(data)
-
+    display = display_data(FILENAME)
 
 def load_data(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(filename)
+    nest = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        nest.append(parts)
     input_file.close()
+    return nest
 
-
+def display_data(filename=FILENAME):
+    input_file = open(filename)
+    for line in input_file:
+        parts = line.strip().split(",")
+        print(f"{parts[0]} is taught by {parts[1]:<12} and has {parts[2]:>3} students")
+    input_file.close()
 main()
