027f941882cbd10df91907911b159cb29acd5929
Edmond <edmond.xie@my.jcu.edu.au>
2025-10-18 02:33:26 +1000

    Display the hex code for the entered colour.

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,1 +1,10 @@
-COLOURS = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "AliceBlue": "#f0f8ff", "Alizarin crimson": "#e32636", "Amaranth": "#e52b50", "Amber": "#ffbf00", "Amethyst": "#9966cc", "Apricot": "#fbceb1", "Aqua": "#00ffff", "Army Green": "#4b5320"}
+COLOURS = {"Asparagus": "#87a96b", "Aureolin": "#fdee00", "Beaver": "#9f8170", "Beige": "#f5f5dc", "Amaranth": "#e52b50", "Amber": "#ffbf00", "Amethyst": "#9966cc", "Apricot": "#fbceb1", "Aqua": "#00ffff", "Bistre": "#3d2b1f"}
+
+choice = input("Colour: ").capitalize()
+while choice != "":
+    if choice in COLOURS:
+        print(f"{choice} is {COLOURS[choice]}")
+        choice = input("Colour: ")
+    else:
+        print("Invalid colour name")
+        choice = input("Colour: ")
