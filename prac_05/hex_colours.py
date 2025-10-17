86958868e59f428bc14197f5f821dcb26e4c776b
Edmond <edmond.xie@my.jcu.edu.au>
2025-10-18 02:22:48 +1000

    Create a list of 10 colours

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,1 @@
+COLOURS = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "AliceBlue": "#f0f8ff", "Alizarin crimson": "#e32636", "Amaranth": "#e52b50", "Amber": "#ffbf00", "Amethyst": "#9966cc", "Apricot": "#fbceb1", "Aqua": "#00ffff", "Army Green": "#4b5320"}
