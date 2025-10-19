22233468358333eb460a52bb408f43f0dc60b2eb
Edmond <edmond.xie@my.jcu.edu.au>
2025-10-19 21:41:43 +1000

    Display emails based on given emails in a dictionary

--- prac_05/email.py
+++ prac_05/email.py
@@ -6,9 +6,12 @@ Actual: 34 minutes
 
 emails = {}
 
+def find_name(given_email):
+    return " ".join(given_email.split("@")[0].split(".")).title()
+
 given_email = input("Email: ")
 while given_email != "":
-    name = given_email.split("@")[0].replace(".", " ").title()
+    name = find_name(given_email)
     confirmation = input(f"Is your name {name}? (Y/n) ").lower()
     if confirmation == "" or confirmation == "y":
         emails[name] = given_email
@@ -17,6 +20,7 @@ while given_email != "":
         given_name = input("Name: ")
         emails[name] = given_email
     given_email = input("Email: ")
+
 
 print()
 for user in emails:
