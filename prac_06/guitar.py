ceec11ff16888ec196cdc5ee28cf91a165409949
Edmond <edmond.xie@my.jcu.edu.au>
2025-11-02 17:06:34 +1000

    Add __str__ method

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,6 +1,10 @@
 class Guitar:
 
-    def __init__(self, name="", year="", cost=""):
+    def __init__(self, name="", year=0, cost=0):
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        return f"{self.name} ({self.year}) : {self.cost}"
+
