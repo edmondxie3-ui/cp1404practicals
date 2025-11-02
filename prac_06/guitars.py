3a6eac455e958ecbea324298208f28b9689634bc
Edmond <edmond.xie@my.jcu.edu.au>
2025-11-02 23:06:38 +1000

    Add while loop to add a guitar

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,2 +1,13 @@
 from prac_06.guitar import Guitar
 
+print("My Guitars!")
+guitars = []
+
+name = input("Name: ")
+while name != "":
+    year = int(input("Year: "))
+    cost = float(input("Cost: $"))
+    guitar = Guitar(name, year, cost)
+    guitars.append(guitar)
+    print(f"{guitar.name} ({guitar.year}) : ${guitar.cost} added.")
+    name = input("Name: ")
