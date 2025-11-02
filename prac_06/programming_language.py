fb2f61d36c2e5ae2c97bb4f96a34a6b5814ec985
Edmond <edmond.xie@my.jcu.edu.au>
2025-11-02 16:20:20 +1000

    Add __init__ method

---
+++ prac_06/programming_language.py
@@ -0,0 +1,8 @@
+class Language:
+
+    def __init__(self, name="", typing="", reflection="", year=""):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
