9187d57dc06919cfa78474540daffc38f629248c
Edmond <edmond.xie@my.jcu.edu.au>
2025-11-02 22:30:55 +1000

    Add testing statements

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -3,5 +3,7 @@ from prac_06.guitar import Guitar
 def main():
     gibson_guitar = Guitar("Gibson L-5 CES", 1922, 16025.40)
     print(gibson_guitar)
+    print(f"Gibson L-5 CES get_age() - Expected 103. Got {gibson_guitar.get_age()}")
+    print(f"Gibson L-5 CES is_vintage() - Expected True. Got {gibson_guitar.is_vintage(gibson_guitar.get_age)}")
 
 main()
