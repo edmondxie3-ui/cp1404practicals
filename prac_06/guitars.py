1fcaf400934c04d7c32e4ca6fae74253fb5966c1
Edmond <edmond.xie@my.jcu.edu.au>
2025-11-02 23:07:13 +1000

    Add the display of each guitar

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -11,3 +11,8 @@ while name != "":
     guitars.append(guitar)
     print(f"{guitar.name} ({guitar.year}) : ${guitar.cost} added.")
     name = input("Name: ")
+
+print("These are my guitars: ")
+for i, guitar in enumerate(guitars, 1):
+    vintage_string = " (vintage)" if guitar.is_vintage(guitar.get_age) else ""
+    print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f} {vintage_string}")
