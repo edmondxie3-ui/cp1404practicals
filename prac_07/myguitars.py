e7f9216a00c585b0e5a05480b85a88527a0eda23
Edmond <edmond.xie@my.jcu.edu.au>
2025-11-09 03:07:20 +1000

    Add guitars from csv

---
+++ prac_07/myguitars.py
@@ -0,0 +1,19 @@
+from prac_07.guitar import Guitar
+
+def main():
+
+    guitars = load_guitars("guitars.csv")
+
+def load_guitars(filename):
+    guitars = []
+    with open(filename, "r") as in_file:
+        for line in in_file:
+            parts = line.strip().split(',')
+            name = parts[0]
+            year = int(parts[1])
+            cost = float(parts[2])
+            guitar = Guitar(name,year, cost)
+            guitars.append(guitar)
+    return guitars
+
+main()
