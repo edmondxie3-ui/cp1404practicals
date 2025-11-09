a84b7c80b00449ac1245fa8eb0ce941885270fef
Edmond <edmond.xie@my.jcu.edu.au>
2025-11-09 13:37:42 +1000

    Add __str__ method

--- prac_07/project.py
+++ prac_07/project.py
@@ -2,12 +2,17 @@ import datetime
 
 class Project:
 
-    def __init__(self, name="", date=datetime, priority=0, cost=0, completion=0):
+    def __init__(self, name="", start_date=datetime, priority=0, cost_estimate=0, completion_percent=0):
         self.name = name
-        self.date = date
+        self.start_date = start_date
         self.priority = priority
-        self.cost = cost
-        self.completion = completion
+        self.cost_estimate = cost_estimate
+        self.completion_percent = completion_percent
+
+    def __str__(self):
+        return (f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, "
+                f"priority {self.priority}, estimate: ${self.cost_estimate:.2f}, "
+                f"completion: {self.completion_percent}%")
 
     def __lt__(self,other):
         return self.priority < other.priority
