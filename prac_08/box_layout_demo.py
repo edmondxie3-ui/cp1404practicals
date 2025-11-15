ed616ee4e2d1f01bbc0521db16baf3c712679183
Edmond <edmond.xie@my.jcu.edu.au>
2025-11-16 03:06:29 +1000

    Update greet message

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -10,6 +10,6 @@ class BoxLayoutDemo(App):
 
     def handle_greet(self):
         print('test')
-        self.root.ids.output_label.text = "Hello "
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
 BoxLayoutDemo().run()
