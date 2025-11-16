c1fa863b4cb7b3e06538613eccf0728f69a687ee
Edmond <edmond.xie@my.jcu.edu.au>
2025-11-16 18:50:25 +1000

    Add string property

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,10 +1,20 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 
 class ConvertMilesKm(App):
+    message = StringProperty()
+
     def build(self):
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+    def handle_calculate(self, value):
+        try:
+            result = float(value) * 1.609
+            self.message = str(result)
+        except ValueError:
+            pass
 
 ConvertMilesKm().run()
